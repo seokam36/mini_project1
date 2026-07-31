f21187b9a87fe9e25d1a6a3b1b254cc36b2d171a
seokam36 <seokam36@gmail.com>
2026-07-31 12:23:36 +0900

    view.html 헤더 수정

--- view.js
+++ view.js
@@ -134,4 +134,19 @@ function applyStudy() {
     localStorage.setItem('studyList', JSON.stringify(studyList));
     alert('신청완료');
     location.reload(); // 화면 새로고침해서 늘어난 인원수 체크
+
+}
+
+let loginUser = JSON.parse(localStorage.getItem('loginUser'))
+
+if(loginUser != 0){
+    document.querySelector('#user').innerHTML = `${loginUser.userName}님 환영합니다! `;
+    document.querySelector('#login').innerHTML = `<a href='' onclick="logout()">ㅤLogout</a>`;
+    document.querySelector("#register").style.display="none";
+}
+
+function logout(){
+    localStorage.removeItem("loginUser");
+    alert("로그아웃 되었습니다.");
+    location.reload();
 }
